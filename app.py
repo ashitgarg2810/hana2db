a29fc2af75b9a0012d9a107344f0a29d0a422978
ashitgarg2810 <ashitgarg2810@gmail.com>
2025-08-27 17:03:24 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,19 +4,15 @@ import time
 import json
 import os
 
---- Databricks credentials from Streamlit secrets ---
 
 host = st.secrets["DATABRICKS_HOST"]
 token = st.secrets["DATABRICKS_TOKEN"]
 job_id = st.secrets["DATABRICKS_JOB_ID"]
 
---- UI Header ---
 
 st.markdown("<h1 style='text-align: center; font-size: 48px;'>Lake Shift</h1>", unsafe_allow_html=True)
 
 uploaded_file = st.file_uploader("Choose a file", type=["txt", "xml"])
-
---- Start Button ---
 
 if uploaded_file is not None and st.button("🚀 Start"):
 # Read file as bytes
