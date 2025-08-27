8bcda11819042fcdfae019e47cbf8797f9128dd6
ashitgarg2810 <ashitgarg2810@gmail.com>
2025-08-27 18:35:09 +0530

    Update app.py

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ token = st.secrets["DATABRICKS_TOKEN"]
 job_id = st.secrets["DATABRICKS_JOB_ID"]
 
 # --- UI Header ---
-st.markdown("<h1 style='text-align: center; font-size: 48px;'>Lake Shift</h1>", unsafe_allow_html=True)
+st.markdown("<h1 style='text-align: center; font-size: 48px;'>LakeShift</h1>", unsafe_allow_html=True)
 
 uploaded_file = st.file_uploader("Choose a file", type=["txt", "xml"])
 
